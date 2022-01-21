5399db567900cf9d4027b9368b59c147777482ae
Maxx <57435414+Maxx-4uTeP@users.noreply.github.com>
2022-01-21 12:25:58 +0300

    Before Community Roles (page 44 done)

--- terraform/prod/ansible_inventory.tf
+++ terraform/prod/ansible_inventory.tf
@@ -1,17 +1,32 @@
 resource "local_file" "generate_inventory" {
-  content = templatefile("../../ansible/inventory.tftpl", {
+  content = templatefile("../../ansible/environments/prod/inventory.tftpl", {
     ip_app = module.app.external_ip_address_app,
     ip_db = module.db.external_ip_address_db
   })
-  filename = "../../ansible/inventory.yml"
+  filename = "../../ansible/environments/prod/inventory.yml"
 
   provisioner "local-exec" {
-    command = "chmod a-x ../../ansible/inventory.yml"
+    command = "chmod a-x ../../ansible/environments/prod/inventory.yml"
   }
 
   provisioner "local-exec" {
     when       = destroy
-    command    = "mv ../../ansible/inventory.yml ../../ansible/inventory.yml.backup"
+    command    = "mv ../../ansible/environments/prod/inventory.yml ../../ansible/environments/prod/inventory.yml.backup"
     on_failure = continue
   }
+}
+
+#db ip ansible config
+resource "local_file" "generate_app_db_config_in_variable" {
+  content = templatefile("../../ansible/environments/prod/group_vars/app.tftpl", {
+    db_host = module.db.internal_ip_address_db
+  })
+  filename = "../../ansible/environments/prod/group_vars/app"
+
+    provisioner "local-exec" {
+    when       = destroy
+    command    = "mv ../../ansible/environments/prod/group_vars/app ../../ansible/environments/prod/group_vars/app.backup"
+    on_failure = continue
+  }
+
 }
--- terraform/stage/ansible_inventory.tf
+++ terraform/stage/ansible_inventory.tf
@@ -1,17 +1,32 @@
 resource "local_file" "generate_inventory" {
-  content = templatefile("../../ansible/inventory.tftpl", {
+  content = templatefile("../../ansible/environments/stage/inventory.tftpl", {
     ip_app = module.app.external_ip_address_app,
     ip_db = module.db.external_ip_address_db
   })
-  filename = "../../ansible/inventory.yml"
+  filename = "../../ansible/environments/stage/inventory.yml"
 
   provisioner "local-exec" {
-    command = "chmod a-x ../../ansible/inventory.yml"
+    command = "chmod a-x ../../ansible/environments/stage/inventory.yml"
   }
 
   provisioner "local-exec" {
     when       = destroy
-    command    = "mv ../../ansible/inventory.yml ../../ansible/inventory.yml.backup"
+    command    = "mv ../../ansible/environments/stage/inventory.yml ../../ansible/environments/stage/inventory.yml.backup"
     on_failure = continue
   }
+}
+
+#db ip ansible config
+resource "local_file" "generate_app_db_config_in_variable" {
+  content = templatefile("../../ansible/environments/stage/group_vars/app.tftpl", {
+    db_host = module.db.internal_ip_address_db
+  })
+  filename = "../../ansible/environments/stage/group_vars/app"
+
+    provisioner "local-exec" {
+    when       = destroy
+    command    = "mv ../../ansible/environments/stage/group_vars/app ../../ansible/environments/stage/group_vars/app.backup"
+    on_failure = continue
+  }
+
 }
